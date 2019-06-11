0617d2910e63fde68d6a8ee8a4ee4674eb9783ef
davidedepaola <39624664+davidedepaola@users.noreply.github.com>
2019-06-11 14:43:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
 });
-server.listen(8080);
+
+app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(name + ' Submitted Successfully!');
+});
+
+var server = app.listen(8080, function () {
+    console.log('Node server is running..');
+});
 
 /*
 //  OpenShift sample Node application
