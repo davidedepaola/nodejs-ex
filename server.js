5362f4ed783a70b4ee4d997443aca1079e403a3f
davidedepaola <39624664+davidedepaola@users.noreply.github.com>
2019-06-11 15:36:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/submit-student-data', function (req, res) {
